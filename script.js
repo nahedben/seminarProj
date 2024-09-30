b4a7b916e9cbd6ae853abe4a7cd1d9e584719d49
Nahed <nahedben@gmail.com>
2024-10-01 00:15:41 +0300

    change display title

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const seminars = [
     title: "Hidden attractors in non-smooth  dynamical systems",
     speaker: {
       name: "هاني البدري بخيت ",
-      grade: "ا.د",
+      grade: "أ.د",
     },
     startHour: "11:00",
     endHour: "13:00",
     day: "الثلاثاء",
     date: "2024-10-15",
-    abstract: "Abstract still not available pr.Hani",
+    abstract: "Abstract still not available",
     Keywords: "",
+    englishName: "Hany Albadrey Bakit",
+    department:
+      "Department of Mathematics, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
     isVisible: "none",
   },
   {
@@ -30,6 +33,9 @@ const seminars = [
       "Although numerous methods have been reported to prepare nanomaterials, there is still a demand for lowering the required preparation time and cost while maintaining suitable material properties for electrochemical energy storage. In this presentation, an overview of our published studies highlights the utilization of cost-effective and simple methods for synthesizing nanomaterials of metal oxides/hydroxides, carbon forms, and conductive polymers. The synthesis methods include microwave-assisted heating, chemical bath deposition, hydrothermal reaction, ball-milling, sonication, electrodeposition, electrospinning, chemical and physical activations, and scree-printing. The relationship between the synthetic parameters and the structural properties is captured using an X-ray diffractometer, scanning electron microscope, transmission electron microscope, and others. The prepared nanomaterials with promising properties have been used to fabricate supercapacitor electrodes in aqueous electrolytes. The electrochemical techniques of cyclic voltammetry (CV), Galvanostatic charge-discharge (GCD), and electrochemical impedance spectroscopy (EIS) have been conducted to investigate the electrical charge storage mechanisms. The electrochemical performance of supercapacitors is evaluated in terms of specific capacitance, energy density, and power density. Most of our supercapacitors are built on flexible substrates such as carbon cloth and stainless-steel mesh, facilitating their future integration into smart textiles and portable electronics.",
     Keywords:
       "nanomaterials; supercapacitor; electrochemical energy; storage devices.",
+    englishName: "Nuha A. Alhebshi",
+    department:
+      "Department of Physics, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
     isVisible: "none",
   },
   {
@@ -44,8 +50,11 @@ const seminars = [
     endHour: "13:00",
     day: "الثلاثاء",
     date: "2024-11-05",
-    abstract: "Abstract still not available Dr. Mahmoud",
+    abstract: "Abstract still not available",
     Keywords: "",
+    englishName: "Mahmoud Saleh",
+    department:
+      "Biology Department, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
     isVisible: "none",
   },
   {
@@ -62,6 +71,9 @@ const seminars = [
     abstract:
       "Corrosion engineering is an expanding discipline focused on understanding, preventing, and controlling material deterioration caused by chemical, electrochemical, or environmental interactions. It is vital for extending the lifespan and ensuring the safety of structures and machinery, with applications in industries such as oil and gas, automotive, construction, and aerospace. Advances in coatings, corrosion inhibitors, and materials science are driving new research and innovation in the field. As global infrastructure ages and sustainability becomes increasingly important, the demand for skilled corrosion engineers will rise, offering numerous opportunities for future scientists to address these critical challenges.",
     Keywords: "",
+    englishName: "Jeenat Aslam",
+    department:
+      "Department of Chemistry, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
     isVisible: "none",
   },
 ];
@@ -118,8 +130,7 @@ const renderSeminars = (arr) => {
               <div class="sem-title" id="seminar-${sem.id}">
                       ${sem.title}
               </div>
-              <div class="name" id="name-${sem.id}"><span lang="ar" direction="ltr">${sem.speaker.grade} </span>.<span lang="ar" direction="ltr">${sem.speaker.name}</span>
-              </div>
+             
             </div> </div>
             <hr>
                 <div class="date">
@@ -138,6 +149,8 @@ const renderSeminars = (arr) => {
                 
               
               <div  id="abst-${sem.id}" style="display:${sem.isVisible}">
+              <h4>${sem.englishName}</h4>
+              <h5>${sem.department}</h5>
               <h3>Abstract</h3>
               <p>${sem.abstract}</p>
               </div>
