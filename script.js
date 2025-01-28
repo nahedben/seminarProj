70d6c653fb5040431ff25256f6c571587360b40f
Nahed <nahedben@gmail.com>
2025-01-28 19:29:09 +0300

    sem2 seminar

--- script.js
+++ script.js
@@ -1,83 +1,151 @@
 const seminars = [
   {
+    // id: 1,
+    // title: "Hidden attractors in non-smooth  dynamical systems",
+    // speaker: {
+    //   name: "هاني البدري بخيت ",
+    //   grade: "أ.د",
+    // },
+    // startHour: "13:00",
+    // endHour: "12:00",
+    // day: "الثلاثاء",
+    // date: "2024-10-15",
+    // abstract:
+    //   "This work aim to investigates the existence of self-excited and hidden attractors in discontinuous systems involving sliding mode.  The hypersurface plays a critical role in allowing rapid transient responses to dynamic phenomena. To show the effect of a hypersurface on the dynamics of a discontinuous system, we present a simple discontinuous system composed of dissipative linear subsystems with no equilibria. Our results show that the periodic orbit can be completely established within a sliding region. We then proceed to a mathematical investigation into a path to self-excited and hidden attractors involving sliding mode in a discontinuous system composed of dissipative Jerk-like subsystems separated by a hypersurface. A Poincaré return map is developed to incorporate the presence of the hypersurface, providing the necessary conditions in the basis for sliding period-doubling orbits that lead to the sliding chaotic attractor. Our results show that various types of attractors and their coexisting attractors, such as sliding period doubling and chaotic behavior, are successfully captured. Using path-following techniques for discontinuous dynamical systems, the results are verified numerically.",
+    // Keywords: "",
+    // englishName: "Hany Albadrey Bakit",
+    // department:
+    //   "Department of Mathematics, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
+    // isVisible: "none",
     id: 1,
-    title: "Hidden attractors in non-smooth  dynamical systems",
+    title: "A study of Riemannian maps in Kaehler geometry",
     speaker: {
-      name: "هاني البدري بخيت ",
+      name: "تنفير فطيمة",
       grade: "أ.د",
     },
-    startHour: "13:00",
-    endHour: "12:00",
+    startHour: "12:00",
+    endHour: "13:00",
     day: "الثلاثاء",
-    date: "2024-10-15",
+    date: "2025-02-04",
     abstract:
-      "This work aim to investigates the existence of self-excited and hidden attractors in discontinuous systems involving sliding mode.  The hypersurface plays a critical role in allowing rapid transient responses to dynamic phenomena. To show the effect of a hypersurface on the dynamics of a discontinuous system, we present a simple discontinuous system composed of dissipative linear subsystems with no equilibria. Our results show that the periodic orbit can be completely established within a sliding region. We then proceed to a mathematical investigation into a path to self-excited and hidden attractors involving sliding mode in a discontinuous system composed of dissipative Jerk-like subsystems separated by a hypersurface. A Poincaré return map is developed to incorporate the presence of the hypersurface, providing the necessary conditions in the basis for sliding period-doubling orbits that lead to the sliding chaotic attractor. Our results show that various types of attractors and their coexisting attractors, such as sliding period doubling and chaotic behavior, are successfully captured. Using path-following techniques for discontinuous dynamical systems, the results are verified numerically.",
+      "This study explores Riemannian maps in the context of Kähler geometry, focusing on conformal Riemannian maps and their subclasses: conformal anti-invariant, semi-invariant, and slant Riemannian maps. Riemannian maps serve as generalizations of isometric immersions and submersions, offering a framework to transfer geometric structures between manifolds. Key properties, such as harmonicity, geodesicness, and umbilicality, are examined, emphasizing the role of dilation in conformal mappings. Applications and theoretical insights are drawn to elucidate the geometry of source and target manifolds in almost Hermitian and Kähler settings.",
     Keywords: "",
-    englishName: "Hany Albadrey Bakit",
+    englishName: "Tanveer Fatima",
     department:
-      "Department of Mathematics, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
+      "Department of Math & Stats College of Sciences, Taibah University, Yanbu",
     isVisible: "none",
   },
   {
     id: 2,
     title:
-      "Nanomaterials-based Supercapacitors Prepared by Cost-effective and Simple Methods",
+      "Insights into Morphological, Molecular, and Histopathological Studies of Parasitic and Zoonotic Species Across Aquatic and Terrestrial Hosts",
     speaker: {
-      name: "نهى علوي الحبشي",
-      grade: "د",
+      name: "عبد الحسان الشازلي  يونس",
+      grade: "أ.د",
     },
-    startHour: "13:00",
-    endHour: "12:00",
+    startHour: "12:00",
+    endHour: "13:00",
     day: "الثلاثاء",
-    date: "2024-10-29",
+    date: "2025-02-11",
     abstract:
-      "Although numerous methods have been reported to prepare nanomaterials, there is still a demand for lowering the required preparation time and cost while maintaining suitable material properties for electrochemical energy storage. In this presentation, an overview of our published studies highlights the utilization of cost-effective and simple methods for synthesizing nanomaterials of metal oxides/hydroxides, carbon forms, and conductive polymers. The synthesis methods include microwave-assisted heating, chemical bath deposition, hydrothermal reaction, ball-milling, sonication, electrodeposition, electrospinning, chemical and physical activations, and scree-printing. The relationship between the synthetic parameters and the structural properties is captured using an X-ray diffractometer, scanning electron microscope, transmission electron microscope, and others. The prepared nanomaterials with promising properties have been used to fabricate supercapacitor electrodes in aqueous electrolytes. The electrochemical techniques of cyclic voltammetry (CV), Galvanostatic charge-discharge (GCD), and electrochemical impedance spectroscopy (EIS) have been conducted to investigate the electrical charge storage mechanisms. The electrochemical performance of supercapacitors is evaluated in terms of specific capacitance, energy density, and power density. Most of our supercapacitors are built on flexible substrates such as carbon cloth and stainless-steel mesh, facilitating their future integration into smart textiles and portable electronics.",
-    Keywords:
-      "nanomaterials; supercapacitor; electrochemical energy; storage devices.",
-    englishName: "Nuha A. Alhebshi",
+      "This presentation explores a series of investigations into the morphology, molecular characterization, and pathogenic impacts of parasitic species across diverse hosts in Egypt and beyond. The research emphasizes zoonotic risks, evolutionary insights, and the implications of parasitic-host interactions. The parasites studied include helminths in Fish (Lake Nasser), bats and house rats. The findings highlighted zoonotic threats and provides a foundation for preventive measures.",
+    Keywords: "",
+    englishName: "Abuelhassan Elshazly Younis",
     department:
-      "Department of Physics, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
+      "Department of Biology College of Sciences, Taibah University, Yanbu",
     isVisible: "none",
+
+    // title:
+    //   "Nanomaterials-based Supercapacitors Prepared by Cost-effective and Simple Methods",
+    // speaker: {
+    //   name: "نهى علوي الحبشي",
+    //   grade: "د",
+    // },
+    // startHour: "13:00",
+    // endHour: "12:00",
+    // day: "الثلاثاء",
+    // date: "2024-10-29",
+    // abstract:
+    //   "Although numerous methods have been reported to prepare nanomaterials, there is still a demand for lowering the required preparation time and cost while maintaining suitable material properties for electrochemical energy storage. In this presentation, an overview of our published studies highlights the utilization of cost-effective and simple methods for synthesizing nanomaterials of metal oxides/hydroxides, carbon forms, and conductive polymers. The synthesis methods include microwave-assisted heating, chemical bath deposition, hydrothermal reaction, ball-milling, sonication, electrodeposition, electrospinning, chemical and physical activations, and scree-printing. The relationship between the synthetic parameters and the structural properties is captured using an X-ray diffractometer, scanning electron microscope, transmission electron microscope, and others. The prepared nanomaterials with promising properties have been used to fabricate supercapacitor electrodes in aqueous electrolytes. The electrochemical techniques of cyclic voltammetry (CV), Galvanostatic charge-discharge (GCD), and electrochemical impedance spectroscopy (EIS) have been conducted to investigate the electrical charge storage mechanisms. The electrochemical performance of supercapacitors is evaluated in terms of specific capacitance, energy density, and power density. Most of our supercapacitors are built on flexible substrates such as carbon cloth and stainless-steel mesh, facilitating their future integration into smart textiles and portable electronics.",
+    // Keywords:
+    //   "nanomaterials; supercapacitor; electrochemical energy; storage devices.",
+    // englishName: "Nuha A. Alhebshi",
+    // department:
+    //   "Department of Physics, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
+    // isVisible: "none",
   },
   {
     id: 3,
-    title:
-      "Growth enhancement of crop plants utilizing plant growth-promoting rhizobacteria",
+    title: "The Nuclear Equation of state and the properties of Neutron stars",
     speaker: {
-      name: "محمود صالح",
+      name: "على المشناب",
       grade: "د",
     },
-    startHour: "13:00",
-    endHour: "12:00",
+    startHour: "12:00",
+    endHour: "13:00",
     day: "الثلاثاء",
-    date: "2024-11-05",
-    abstract:
-      "The improvement of crop yield using plant growth-promoting rhizobacteria (PGPR) represents a sustainable and environmentally friendly strategy to enhance agricultural production over the long term. PGPR are soil bacteria that colonize plant roots and inhabit the rhizosphere, where they can exist as neutral, beneficial or harmful organisms. These beneficial microbes facilitate improved water absorption, suppress pathogenic organisms and enhance nutrient uptake from the soil. In addition, various PGPR species have been identified to mitigate the harmful effects of salinity stress, thereby increasing plant stress tolerance. This lecture discusses some biochemical mechanisms through which PGPR stimulate plant growth including (1) bio-stimulants, including specific phytohormones such as auxins (e.g. indole acetic acid, cytokinin, gibberellic acid, and ethylene); (2) biofertilization, which aids in the uptake of essential nutrients through processes such as biological nitrogen fixation, phosphate solubilization, and siderophore production; and (3) bioprotectants or biocontrol agents, which mitigate plant diseases vis the production of antibiotics, lytic enzymes, and hydrogen cyanide. Furthermore, new approach was employed to increase the efficacy of PGPR, the utilization of He-Ne laser radiation has been recently explored. Laser treatment can enhance the activity of PGPR by stimulating metabolic processes, improving the colonization of plant roots, and promising the expression of beneficial traits in the bacteria. This combination of laser technology and PGPR application holds promise for optimizing plant growth and resilience, particularly in challenging environmental conditions.",
+    date: "2025-04-29",
+    abstract: "",
     Keywords: "",
-    englishName: "Mahmoud Saleh",
+    englishName: "Alaa Elmeshneb",
     department:
-      "Biology Department, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
+      "Department of Physics College of Sciences, Taibah University, Yanbu",
     isVisible: "none",
+    // title:
+    //   "Growth enhancement of crop plants utilizing plant growth-promoting rhizobacteria",
+    // speaker: {
+    //   name: "محمود صالح",
+    //   grade: "د",
+    // },
+    // startHour: "13:00",
+    // endHour: "12:00",
+    // day: "الثلاثاء",
+    // date: "2024-11-05",
+    // abstract:
+    //   "The improvement of crop yield using plant growth-promoting rhizobacteria (PGPR) represents a sustainable and environmentally friendly strategy to enhance agricultural production over the long term. PGPR are soil bacteria that colonize plant roots and inhabit the rhizosphere, where they can exist as neutral, beneficial or harmful organisms. These beneficial microbes facilitate improved water absorption, suppress pathogenic organisms and enhance nutrient uptake from the soil. In addition, various PGPR species have been identified to mitigate the harmful effects of salinity stress, thereby increasing plant stress tolerance. This lecture discusses some biochemical mechanisms through which PGPR stimulate plant growth including (1) bio-stimulants, including specific phytohormones such as auxins (e.g. indole acetic acid, cytokinin, gibberellic acid, and ethylene); (2) biofertilization, which aids in the uptake of essential nutrients through processes such as biological nitrogen fixation, phosphate solubilization, and siderophore production; and (3) bioprotectants or biocontrol agents, which mitigate plant diseases vis the production of antibiotics, lytic enzymes, and hydrogen cyanide. Furthermore, new approach was employed to increase the efficacy of PGPR, the utilization of He-Ne laser radiation has been recently explored. Laser treatment can enhance the activity of PGPR by stimulating metabolic processes, improving the colonization of plant roots, and promising the expression of beneficial traits in the bacteria. This combination of laser technology and PGPR application holds promise for optimizing plant growth and resilience, particularly in challenging environmental conditions.",
+    // Keywords: "",
+    // englishName: "Mahmoud Saleh",
+    // department:
+    //   "Biology Department, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
+    // isVisible: "none",
   },
   {
     id: 4,
-    title: "Corrosion Engineering: A Growing Field for Future Scientists",
+
+    title: "A Quarter Century of Research on Chitosan",
     speaker: {
-      name: "جينات أسلن",
+      name: "خالد خليل",
       grade: "د",
     },
-    startHour: "13:00",
-    endHour: "12:00",
+    startHour: "12:00",
+    endHour: "13:00",
     day: "الثلاثاء",
-    date: "2024-12-03",
+    date: "2025-05-06",
     abstract:
-      "Corrosion engineering is an expanding discipline focused on understanding, preventing, and controlling material deterioration caused by chemical, electrochemical, or environmental interactions. It is vital for extending the lifespan and ensuring the safety of structures and machinery, with applications in industries such as oil and gas, automotive, construction, and aerospace. Advances in coatings, corrosion inhibitors, and materials science are driving new research and innovation in the field. As global infrastructure ages and sustainability becomes increasingly important, the demand for skilled corrosion engineers will rise, offering numerous opportunities for future scientists to address these critical challenges.",
+      "At the end of the 20th century, I found myself drawn to the emerging field of green chemistry. It was an exciting time, with a growing focus on sustainability and reducing the environmental impact of chemical processes. Over the past 25 years, I’ve dedicated my research to creating greener, safer, and more efficient ways to approach chemistry. Whether it’s developing non-toxic catalysts, exploring renewable feedstocks, or designing biodegradable materials, my goal has always been to align science with sustainability. A big part of this journey has been my work with chitosan, a natural biopolymer with some truly remarkable qualities: biodegradability, and antimicrobial activity, to name a few. I’ve published over 30 papers on the subject, exploring its potential applications in pharmaceuticals, agriculture, and environmental science. My research has focused on creating innovative materials like drug delivery systems, bioactive coatings, water purification agents, and nano-catalysts for organic transformations. I’ve also worked on modifying chitosan to make it even more efficient and adaptable for specific uses. At its core, my work is about finding sustainable solutions and pushing the boundaries of what this versatile material can do.",
     Keywords: "",
-    englishName: "Jeenat Aslam",
+    englishName: "Khaled Khalil",
     department:
-      "Department of Chemistry, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
+      "Department of Chemistry, College of Sciences, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
     isVisible: "none",
   },
+  //   title: "Corrosion Engineering: A Growing Field for Future Scientists",
+  //   speaker: {
+  //     name: "جينات أسلن",
+  //     grade: "د",
+  //   },
+  //   startHour: "13:00",
+  //   endHour: "12:00",
+  //   day: "الثلاثاء",
+  //   date: "2024-12-03",
+  //   abstract:
+  //     "Corrosion engineering is an expanding discipline focused on understanding, preventing, and controlling material deterioration caused by chemical, electrochemical, or environmental interactions. It is vital for extending the lifespan and ensuring the safety of structures and machinery, with applications in industries such as oil and gas, automotive, construction, and aerospace. Advances in coatings, corrosion inhibitors, and materials science are driving new research and innovation in the field. As global infrastructure ages and sustainability becomes increasingly important, the demand for skilled corrosion engineers will rise, offering numerous opportunities for future scientists to address these critical challenges.",
+  //   Keywords: "",
+  //   englishName: "Jeenat Aslam",
+  //   department:
+  //     "Department of Chemistry, College of Science, Taibah University, Yanbu, Al-Madina, Saudi Arabia",
+  //   isVisible: "none",
+  // },
 ];
 const bandColors = [
   "edge yellow",
@@ -154,7 +222,9 @@ const renderSeminars = (arr) => {
               <h4>${sem.englishName}</h4>
               <h5>${sem.department}</h5>
               <h3>Abstract</h3>
-              <p>${sem.abstract}</p>
+              <p> ${sem.abstract}</p>
+              
+             
               </div>
           
           </div>
